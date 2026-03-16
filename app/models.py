6394e918b977be67af0e8c21a5ca24fda974e011
Rudra Chhangani <rudrachhangani@email.com>
2026-03-17 00:43:02 +0530

    feat: add follow, unfollow and get followers endpoints

--- app/models.py
+++ app/models.py
@@ -22,4 +22,14 @@ class Post(Base):
     content = Column(String, nullable=False)
     user_id = Column(Integer, nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+class Follow(Base):
+    __tablename__ = "follows"
+
+    id  = Column(Integer, primary_key=True, index = True)
+    follower_id  = Column(Integer, nullable = False)
+    following_id = Column(Integer, nullable = False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    
+
     
